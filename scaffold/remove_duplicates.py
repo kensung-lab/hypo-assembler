561234d4d8bd7481ba353d0757a8fd7a43fb41ba
Joshua Casey <joshua.caseyd@gmail.com>
2025-04-24 16:53:28 +0800

    reworked libraries inconsistency

--- scaffold/remove_duplicates.py
+++ scaffold/remove_duplicates.py
@@ -36,11 +36,49 @@ for align in aligns:
             if total_clips == 0 or total_clips <= 0.01 * align.infer_read_length():
                 fully_contained_contigs.add(qn)
 
-write_contigs = []
+
+
+sorted_contigs = []
 for qn in contigs:
     if qn not in fully_contained_contigs:
-        write_contigs.append(contigs[qn])
+        sorted_contigs.append((qn, str(contigs[qn].seq)))
+sorted_contigs.sort()
+
+final_contigs = []
+previous_qn = None
+current_contig = ""
+hap_code = "_1"
+
+count_removed = 0
+for qn, get_contig in sorted_contigs:
+    if previous_qn is None:
+        previous_qn = qn
+        current_contig = get_contig
+    else:
+        a = "_".join(qn.split("_")[:-2])
+        b = "_".join(previous_qn.split("_")[:-2])
+        
+        hap_code += qn.split("_")[-1]
+        
+        if a == b:
+            current_contig += get_contig
+            count_removed += 1
+        else:
+            final_contigs.append(current_contig)
+            previous_qn = qn
+            current_contig = get_contig
+
+
+if previous_qn is not None:
+    final_contigs.append(current_contig)
+    
+cid = 0
+write_contigs = []
+for contig in final_contigs:
+    write_contigs.append(SeqRecord(Seq(contig), id="contig_%d_%s" % (cid, hap_code), description=""))
+    cid += 1
 
 print("Removed ", len(contigs) - len(write_contigs), " duplicated contigs")
-
+    
+    
 SeqIO.write(write_contigs, output_path, "fasta")
